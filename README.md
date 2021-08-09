e51a50637f0af70a7f716c931b82b989face344b
むずでょ <muzudho1@gmail.com>
2021-08-09 20:54:46 +0900

    CSA読取

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # shogi-watch-view
+
 TODO CSA形式をパースして、残り時間を表示したいぜ（＾～＾）
+
+## Run
+
+```shell
+python main.py
+```
+
+## References
+
+[PythonでWeb上のデータを読み込む：urlopen()](https://uxmilk.jp/23004)  
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import urllib.request as urlreq
+
 # TODO 棋譜ファイル（CSA形式）を読む
+f = urlreq.urlopen('https://golan.sakura.ne.jp/denryusen/dr2_tsec/kifufiles/dr2tsec+buoy_james8nakahi_dr2b3-11-bottom_43_dlshogi_xylty-60-2F+dlshogi+xylty+20210718131042.csa')
+csa = f.read().decode("utf8")
+print(csa) # 開いたファイルの中身を表示する
+f.close()
 
 # TODO タイマーのルール取得（持ち時間、秒読み、加算時間など）
 
