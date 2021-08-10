93d695abdd3ad1a39fc37939067addbd80c3934d
むずでょ <muzudho1@gmail.com>
2021-08-10 13:56:33 +0900

    改行スプリット（＾～＾）

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ import urllib.request as urlreq
 # TODO 棋譜ファイル（CSA形式）を読む
 f = urlreq.urlopen('https://golan.sakura.ne.jp/denryusen/dr2_tsec/kifufiles/dr2tsec+buoy_james8nakahi_dr2b3-11-bottom_43_dlshogi_xylty-60-2F+dlshogi+xylty+20210718131042.csa')
 csa = f.read().decode("utf8")
-print(csa) # 開いたファイルの中身を表示する
+# print(csa) # 開いたファイルの中身を表示する
 f.close()
 
+for line in csa.split('\n'):
+    print(f"> {line}")
+
 # TODO タイマーのルール取得（持ち時間、秒読み、加算時間など）
+# 加算時間
+# 'Increment:2
 
 # TODO 開始時刻を取得
 
