f10f0e964056db1165a6de6ef69f9e6cf1c113e9
むずでょ <muzudho1@gmail.com>
2021-08-10 19:05:58 +0900

    加算時間を忘れてるぜ（＾～＾）

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
-import urllib.request as urlreq
-import re
-import datetime
 from csa_file import CsaFile
 
 # TODO 棋譜ファイル（CSA形式）を読む
 csaFile = CsaFile.load('https://golan.sakura.ne.jp/denryusen/dr2_tsec/kifufiles/dr2tsec+buoy_james8nakahi_dr2b3-11-bottom_43_dlshogi_xylty-60-2F+dlshogi+xylty+20210718131042.csa')
 
-# TODO タイマーのルール取得（持ち時間、秒読み、加算時間など）
-# 持ち時間
-print(f'TimeLimit [0]{csaFile.timeLimit[0]} [1]{csaFile.timeLimit[1]} [2]{csaFile.timeLimit[2]}')
-# 加算時間
-print(f'Increment {csaFile.increment}')
+# 残り時間（秒） ＝ 持ち時間（秒） - 消費時間（秒）
+remaining = [0,
+             csaFile.timeLimit[1] - csaFile.erapsed[1],
+             csaFile.timeLimit[2] - csaFile.erapsed[2]]
+print(f'Remaining [1]{remaining[1]} [2]{remaining[2]}')
 
-# TODO 開始時刻を取得
-print(f'StartTime Y={csaFile.startTime.year} M={csaFile.startTime.month} D={csaFile.startTime.day} H={csaFile.startTime.hour} m={csaFile.startTime.minute} s={csaFile.startTime.second}')
-
-# TODO 指し手の消費時間を全て読取り、累計
-print(f'Erapsed [0]{csaFile.erapsed[0]} [1]{csaFile.erapsed[1]} [2]{csaFile.erapsed[2]}')
-
-# TODO 手番の判定
+# 手番
+print(f'Phase {csaFile.phase}')
 
 # TODO 時計の表示（カウントダウンなどを描画）
