6e859ec815598138160f9fb9242278c07b48f7f3
むずでょ <muzudho1@gmail.com>
2021-08-10 14:26:10 +0900

    消費時間

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ csa = f.read().decode("utf8")
 # print(csa) # 開いたファイルの中身を表示する
 f.close()
 
-# 手番。+が先手、-が後手
-phase = None
-# +2726FU
+# 手番。1が先手、2が後手。配列の添え字に使う
+phase = 0
+# Example: +2726FU
 patternPhase = re.compile(r"^([+-])\d{4}\w{2}$")
 
 # 加算時間
@@ -17,11 +17,28 @@ increment = 0
 # Example: 'Increment:2
 patternIncrement = re.compile(r"^'Increment:(\d+)$")
 
+# 消費時間
+erapsed = [0,0,0]
+# Example: T2
+patternErapsed = re.compile(r"^T(\d+)$")
+
 for line in csa.split('\n'):
     result = patternPhase.match(line)
     if result:
         print(f"Phase {result.group(1)}")
-        phase = result.group(1)
+        sign = result.group(1)
+        if sign=='+':
+            phase = 1
+        elif sign=='-':
+            phase = 2
+        else:
+            phase = 0 # Error
+        continue
+
+    result = patternErapsed.match(line)
+    if result:
+        print(f"Erapsed {result.group(1)}")
+        erapsed[phase] += int(result.group(1))
         continue
 
     result = patternIncrement.match(line)
@@ -39,6 +56,7 @@ for line in csa.split('\n'):
 # TODO 開始時刻を取得
 
 # TODO 指し手の消費時間を全て読取り、累計
+print(f'Erapsed [0]{erapsed[0]} [1]{erapsed[1]} [2]{erapsed[2]}')
 
 # TODO 手番の判定
 
